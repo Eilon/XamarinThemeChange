06dd6750d64cea87d9f7b07eca2f0aa12151bb3a
Eilon Lipton <Eilon@users.noreply.github.com>
2020-10-16 15:16:58 -0700

    Initial commit

---
+++ XamarinThemeChange/XamarinThemeChange/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinThemeChange
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+
+            RequestedThemeChanged += Current_RequestedThemeChanged;
+        }
+
+        int num;
+
+        private void Current_RequestedThemeChanged(object sender, AppThemeChangedEventArgs e)
+        {
+            ((MainPage)MainPage).ThemeUpdates.Add($"{num}: {e.RequestedTheme}");
+            num++;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ XamarinThemeChange/XamarinThemeChange/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinThemeChange
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = this;
+        }
+
+        public ObservableCollection<string> ThemeUpdates { get; } = new ObservableCollection<string>();
+        public string CurrentTheme => $"Current theme: {Application.Current.RequestedTheme}";
+    }
+}
